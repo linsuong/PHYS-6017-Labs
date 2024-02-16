92424e85b234ab9510a240160e364b18d77ccff4
Linus <65358174+linsuong@users.noreply.github.com>
2024-02-16 14:37:01 +0000

    files + start on pendulum proj

---
+++ Projects/ForcedSimplePendulum/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def oscillation(theta, t, k, g, omega, eta):
+    #parameters:
+    alpha = (k/((m * L) * np.sqrt(g * L)))
+    beta = F / (m * g)
+    
